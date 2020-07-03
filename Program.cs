bdf26bb07f46b42c415ac7746c5943bee00e12f5
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:46:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] check = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int number = Int32.Parse(Console.ReadLine());
+            if (number <= check.Length)
+            {
+                string found = check[number-1];
+                Console.WriteLine(found);
+            }
+            else
+            {
+                Console.WriteLine("N/A");
+            }
+
+        }
+    }
+}
